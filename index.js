8263ec2629c3a73472a1b4dba6e746ddc0e50acf
Brigid Wanza Wambua <wanzanashami@gmail.com>
2024-03-29 18:17:46 +0300

    feat: comleted

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+// Define num1 and num2
+var num1 = 31;
+var num2 = 2;
+
+// Create a variable called multiply and set it to the result of multiplying num1 and num2
+var multiply = num1 * num2;
+
+// Check if the result is 62
+console.log(multiply === 62); // Output: true
