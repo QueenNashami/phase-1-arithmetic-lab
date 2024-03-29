2dff1a9a95578878f6c534ee92cf1c7c53ea7df8
Brigid Wanza Wambua <wanzanashami@gmail.com>
2024-03-29 18:56:16 +0300

    refact: changed from using a set to math.max directcly

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
 // Write your code here
-var num1 = 31;
-var num2 = 2;
-var multiply = num1 * num2;
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 let random = Math.floor(Math.random() * 10) + 1;
 let mod;
 let num3 = 104
 let num4 = 5;
 mod = num3 % num4
 let max;
-let setData = new Set([2, 4, 5, 20, 12]);
-max = Math.max(...setData)
+max = Math.max(2, 6, 7, 20, 14)
